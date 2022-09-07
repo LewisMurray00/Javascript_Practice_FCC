da3100ca11567e8b4f756c731d692c953929eea2
Lewis Murray <lewisdmurray00@gmail.com>
2022-09-07 18:52:23 +0100

    information is dynamically pulled from an array as opposed to being hard coded in

--- 03-Reviews/app.js
+++ 03-Reviews/app.js
@@ -53,5 +53,9 @@ let currentItem = 0;
 
 // Loading initial item
 window.addEventListener("DOMContentLoaded", function(){
-    console.log("hi")
+    const item = reviews[currentItem]
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text
 })
