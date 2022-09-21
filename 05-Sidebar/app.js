5da5c91a1ef82dcef480bb0cff8c01486ae2314e
Lewis Murray <lewisdmurray00@gmail.com>
2022-09-21 21:26:08 +0100

    Finished styling the CSS, had to work my way through a problemon the button not working (this was a CSS error)

--- 05-Sidebar/app.js
+++ 05-Sidebar/app.js
@@ -1,13 +1,13 @@
-const toggleBtn = document.querySelector(".sidebar-toggle")
-const closeBtn = document.querySelector(".close-btn")
-const sidebar = document.querySelector(".sidebar")
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
 
-//Toggle function using eventlistener
-toggleBtn.addEventListener("click", function(){
-    sidebar.classList.toggle("show-sidebar")
-})
+toggleBtn.addEventListener("click", function () {
+    console.log("hi")
+  sidebar.classList.toggle("show-sidebar");
+});
 
-// Close function using EventListener
-closeBtn.addEventListener("click", function(){
-    sidebar.classList.toggle("show-sidebar")
-})
+closeBtn.addEventListener("click", function () {
+    console.log("hi")
+  sidebar.classList.remove("show-sidebar");
+});
