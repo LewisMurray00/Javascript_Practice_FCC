bf4125d55b051f5deeec372ec72be3453a346fb9
Lewis Murray <lewisdmurray00@gmail.com>
2022-09-23 18:21:04 +0100

    Added the JS to the project so that the modal now functions properly

--- 06-Modal/app.js
+++ 06-Modal/app.js
@@ -4,6 +4,12 @@ const closeBtn = document.querySelector(".btn-close");
 const modalOverlay = document.querySelector(".modal-overlay");
 
 // Listen for click events on btn-modal and btn-close
-// When user click btn-modal add .Open-modal class to modal-overlay
-// When user clicks btn-close remove .open-modal from modal-overlay
 
+// When user click btn-modal add .Open-modal class to modal-overlay
+modalBtn.addEventListener('click', function(){
+    modalOverlay.classList.add('open-modal')
+})
+// When user clicks btn-close remove .open-modal from modal-overlay
+closeBtn.addEventListener('click', function(){
+    modalOverlay.classList.remove('open-modal')
+})
