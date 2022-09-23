b960acb25b222005083dc6a21eaa0fcda6195029
Lewis Murray <lewisdmurray00@gmail.com>
2022-09-23 18:15:32 +0100

    selected the required elements and assigned them to a variable

---
+++ 06-Modal/app.js
@@ -0,0 +1,9 @@
+// Select modal button, modal overlay and close button
+const modalBtn = document.querySelector(".btn-modal");
+const closeBtn = document.querySelector(".btn-close");
+const modalOverlay = document.querySelector(".modal-overlay");
+
+// Listen for click events on btn-modal and btn-close
+// When user click btn-modal add .Open-modal class to modal-overlay
+// When user clicks btn-close remove .open-modal from modal-overlay
+
