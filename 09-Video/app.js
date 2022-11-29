716632a51fa2aadcdccae90cc4ca79cc82d27fe7
Lewis Murray <lewisdmurray00@gmail.com>
2022-11-29 22:55:20 +0000

    Added a preloader gif, to play whilst the video loads

--- 09-Video/app.js
+++ 09-Video/app.js
@@ -5,8 +5,10 @@ const video = document.querySelector(".video-container");
 // Add a callback function to the button to see if it has the class ".slide"
 btn.addEventListener('click', function(){
     if(!btn.classList.contains('slide')){
-        btn.classList.add("slide")
+        btn.classList.add("slide");
+        video.pause();
     } else{
-        btn.classList.remove("slide")
+        btn.classList.remove("slide");
+        video.play();
     }
 })
