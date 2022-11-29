eba6d77fb73487edef1be91ec0bdabfa493312fb
Lewis Murray <lewisdmurray00@gmail.com>
2022-11-29 22:50:04 +0000

    Added a callback function in JS that checks if the class slide is not present, if it doesnt then it will be added or else removed

---
+++ 09-Video/app.js
@@ -0,0 +1,12 @@
+// Selecting the elements needed across the DOM
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+// Add a callback function to the button to see if it has the class ".slide"
+btn.addEventListener('click', function(){
+    if(!btn.classList.contains('slide')){
+        btn.classList.add("slide")
+    } else{
+        btn.classList.remove("slide")
+    }
+})
