92c9c1e87961464b4b0d7cfc2a6845674a26a343
Lewis Murray <lewisdmurray00@gmail.com>
2022-12-07 17:04:35 +0000

    Added a function that takes into account how far the screen s is scrolled and if it is past a certain length then the class fixed nav will be added

--- 10-Scroll/app.js
+++ 10-Scroll/app.js
@@ -1,3 +1,36 @@
-// Set up the date for the copyright
-const date = document.getElementById("date");
-date.innerHTML = new Date().getFullYear();
+// ******** Set up the date for the copyright ********
+// const date = document.getElementById("date");
+// date.innerHTML = new Date().getFullYear();
+
+// ************ Close links ************
+
+//Selecting the nav-toggle button/class across the DOM
+const navToggle = document.querySelector('.nav-toggle');
+
+//Selecting the links-container class across the DOM
+const linksContainer = document.querySelector('.links-container');
+
+//Selecting the links class across the DOM
+const links = document.querySelector('.links');
+
+//Adding a callback function to show the link container as opposed to it being hidden
+navToggle.addEventListener('click', function(){
+    linksContainer.classList.toggle("show-links")
+})
+
+// Creating a fixed navbar
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('top-link');
+
+window.addEventListener('scroll', function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if(scrollHeight > navHeight){
+        navbar.classList.add('fixed-nav')
+    } else{
+        navbar.classList.remove('fixed-nav')
+    };
+
+
+})
