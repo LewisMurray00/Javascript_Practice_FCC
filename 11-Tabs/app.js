b28755e808b139211e0f218b8452862b5d2f86ad
Lewis Murray <lewisdmurray00@gmail.com>
2022-12-29 20:00:24 +0000

    Next project finito, onto countdown

--- 11-Tabs/app.js
+++ 11-Tabs/app.js
@@ -7,3 +7,21 @@ const btns = document.querySelectorAll('.tab-btn');
 // Get all the articles
 const articles = document.querySelectorAll(".content");
 
+// Listen for a click event
+about.addEventListener('click', function(e){
+    const id = e.target.dataset.id; 
+
+    if(id){
+        // Remove active class from other buttons
+        btns.forEach(function(btn){
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+        // hide other articles
+        articles.forEach(function(article){
+            article.classList.remove('active');
+        })
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+})
