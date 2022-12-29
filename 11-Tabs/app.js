675b9d1fa971c02050ca450cee94ea001f8d4736
Lewis Murray <lewisdmurray00@gmail.com>
2022-12-29 19:48:25 +0000

    Added some JS, by linking things across the DOM

---
+++ 11-Tabs/app.js
@@ -0,0 +1,9 @@
+// Selecting the about section
+const about = document.querySelector('.about');
+
+// Target all the buttons
+const btns = document.querySelectorAll('.tab-btn');
+
+// Get all the articles
+const articles = document.querySelectorAll(".content");
+
