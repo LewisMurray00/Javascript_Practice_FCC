aa492e1571c3e56d5dcf394d1040e35d62405390
Lewis Murray <lewisdmurray00@gmail.com>
2023-01-04 20:48:52 +0000

    Added some variables that are attached to the html elements

---
+++ 14-groceryBud/app.js
@@ -0,0 +1,18 @@
+// ******** Select items ************
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submit = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+// Edit options
+
+// ******** Event Listeners *********
+
+// ******** Functions *********
+
+// ******** Local Storage *********
+
+// ******** Setup Items *********
